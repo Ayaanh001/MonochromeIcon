db41cd64c9d781b291f3d92b0751427af8a6d579
Ayaan Hussain <kingaah83@gmail.com>
2026-05-12 16:37:21 +0530

    Update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,7 +42,6 @@ android {
         compose = true
     }
 
-    // 👇 Required for Kotlin 2.0 + Compose compiler
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.14"
     }
@@ -62,12 +61,12 @@ dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.activity:activity-compose:1.9.2")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.5")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.5")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("com.google.android.material:material:1.12.0")
-    //implementation("androidx.compose.material3:material3")
-    implementation ("androidx.compose.material3:material3:1.2.1")
-    implementation ("androidx.compose.material3:material3-window-size-class:1.2.1")
+    implementation ("androidx.compose.material3:material3:1.3.0")
+    implementation ("androidx.compose.material3:material3-window-size-class:1.3.0")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
